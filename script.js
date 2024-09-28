aecb3110c37cd3bbb1a122f823afcd895901f15b
samiam120 <samassefa3@gmail.com>
2024-09-27 19:13:31 -0700

    basic functionality of tic tac toe in console - can select placement of marker using row,col - console display of boards of each round

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //board logic / adding x's and o's
-const gameBoard = (function () {
+function gameBoard() {
   const rows = 3;
   const cols = 3;
   const board = [];
@@ -11,58 +11,78 @@ const gameBoard = (function () {
       board[i].push(" ");
     }
   }
-
   //getBoard()
   const getBoard = () => board;
 
   //place down marker on available spot
   const placeMarker = (row, col, player) => {
-    if(board[row][col] == " "){
-        //place marker here and update board
-        board[row][col] = player.getMarker();
-    }else{
-        alert("invalid move");
+    if (board[row][col] == " ") {
+      //place marker here and update board
+      board[row][col] = player.getMarker();
+      return true;
+    } else {
+      alert("invalid move");
+      return false;
     }
   };
-  //updateCell()
 
   //printing out our current board to UI
   const printBoard = () => console.log(board);
 
   return { getBoard, placeMarker, printBoard };
-})();
-
-function createPlayer(name, token){
-    let playerName = name;
-    let marker = token;
-
-    const getName = () => playerName;
-    const getMarker = () => marker;
-
-    return {getName, getMarker};
 }
 
+function createPlayer(name, token) {
+  let playerName = name;
+  let marker = token;
+
+  const getName = () => playerName;
+  const getMarker = () => marker;
+
+  return { getName, getMarker };
+}
 
 //cell object --> individual cell and value
 
-function createCell(player, token){
-    let val = token;
+function createCell(player, token) {
+  let val = token;
 
-    const getValue = () => val;
+  const getValue = () => val;
 
-    return{getValue};
+  return { getValue };
 }
 
-//consists of a player
-//marker
+function gameController() {
+  //game controller object
+  //gameboard
+  const board = gameBoard();
+  //players
+  const playerOne = createPlayer("Player One", "X");
+  const playerTwo = createPlayer("Player Two", "O");
+  //current player active
 
-//retrieve cell value
+  let activePlayer = playerOne;
 
-//game controller object
-//gameboard
-//players
-//current player active
-// tie
-// win
+  const getActivePlayer = () => activePlayer;
 
-//controlling rthe flow and state of game
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+  };
+
+  const printBoard = () => board.printBoard();
+  const playRound = (row, col) => {
+    console.log(`${activePlayer.getName()}'s turn....`);
+    if (board.placeMarker(row, col, getActivePlayer())) {
+      switchPlayerTurn();
+      printBoard();
+    } else {
+      alert(`select valid move`);
+    }
+  };
+
+  // tie
+  // win
+
+  //controlling rthe flow and state of game
+  return { getActivePlayer, playRound };
+}
